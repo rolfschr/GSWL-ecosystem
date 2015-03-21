878aef98d26df2a0ae05b294ed8c36d951bd0c64
Rolf Schroder <rolf.schr@gmail.com>
2015-03-21 17:47:03 +0100

    Make reports.py behave correctly

--- reports.py
+++ reports.py
@@ -7,14 +7,42 @@ Usage:
 
 import sys
 import os
+import tty
+import termios
+import subprocess
 
 REPORT_FILE = 'reports.txt'
+TEMP_SCRIPT_FILE = '/tmp/.ledgerscript.sh'
+
+def getchar():
+    # from http://code.activestate.com/recipes/134892/
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def makescript(cmd):
+    out = ""
+    out += '#!/bin/bash\n'
+    out += 'shopt -s expand_aliases\n'
+    out += '. alias\n'
+    out += '{}\n'.format(cmd)
+    with open(TEMP_SCRIPT_FILE, 'w') as fh:
+        fh.write(out)
+    os.chmod(TEMP_SCRIPT_FILE, 0777)
 
 
 def show((expl, cmd)):
+    makescript(cmd)
     os.system('clear')
     print(expl)
     print(cmd)
+    subprocess.call(TEMP_SCRIPT_FILE, shell=True)
 
 
 def main(argv=None):
@@ -39,17 +67,23 @@ def main(argv=None):
                 cmd = ''
                 expl = ''
 
+    cmd = 'bash -s "<<EOF\nsource alias\nled bal\nEOF"'
+    cmd = 'bash -c "source alias; top -n 1; led"'
+    #cmd = 'bash -c "shopt -s expand_aliases; . alias; top -n 1; led"'
+    cmd = 'bash -c "shopt -s expand_aliases; . alias; echo "asdf"; led bal"'
+    #subprocess.call(cmd, shell=True)
+    #return
     i = 0
     while True:
         show(reports[i])
-        action = raw_input('h = previous report, l = next report, q = quit [h/l/q] ? ')
+        print('h = previous report, l = next report, q = quit [h/l/q] ?')
+        action = getchar()
         if (action == 'h'):
             i = i - 1 if i > 0 else len(reports) - 1
         elif (action == 'l'):
             i = i + 1 if i < len(reports) - 1 else 0
         elif (action == 'q'):
             break
-    cmd = 'bash -c "source alias && led bal"'
 
 if __name__ == '__main__':
     main(sys.argv)
